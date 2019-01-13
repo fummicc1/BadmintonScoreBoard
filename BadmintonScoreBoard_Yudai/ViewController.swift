23fb6034c81b7d876f5f0ad0895b75ef33e02a30
fummicc1-sub <fumiya989@gmail.com>
2019-01-13 14:17:32 +0900

    commit

--- BadmintonScoreBoard_Yudai/ViewController.swift
+++ BadmintonScoreBoard_Yudai/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var leftPointScore = 0
+    var leftGameScore = 0
+    
+    var rightPointScore = 0
+    var rightGameScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tappedLeftButton(sender: UIButton) {
+        leftPointScore += 1
+        sender.setTitle(String(leftPointScore), for: UIControl.State.normal)
+    }
+    
+    @IBAction func tappedRightButton(sender: UIButton) {
+        rightPointScore += 1
+        sender.setTitle(String(rightPointScore), for: UIControl.State.normal)
+    }
 
+    func checkGameset() {
+//        if leftPointScore >= 
+    }
+    
 }
 
