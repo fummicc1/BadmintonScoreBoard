8bd2477c4a61aee4da9957d73c1eaf5263863b52
fummicc1-sub <fumiya989@gmail.com>
2019-01-14 09:38:24 +0900

    アラートまで完了

--- BadmintonScoreBoard_Yudai/ViewController.swift
+++ BadmintonScoreBoard_Yudai/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var rightPointScore = 0
     var rightGameScore = 0
+    var distance: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +25,50 @@ class ViewController: UIViewController {
     @IBAction func tappedLeftButton(sender: UIButton) {
         leftPointScore += 1
         sender.setTitle(String(leftPointScore), for: UIControl.State.normal)
+        checkGameset()
     }
+    
     
     @IBAction func tappedRightButton(sender: UIButton) {
         rightPointScore += 1
         sender.setTitle(String(rightPointScore), for: UIControl.State.normal)
+        checkGameset()
     }
 
     func checkGameset() {
-//        if leftPointScore >= 
+        
+        if leftPointScore == 21, rightPointScore <= 19 {
+            print("左側の選手が勝利です。")
+        } else if rightPointScore == 21, leftPointScore <= 19 {
+            print("右側の選手が勝利です。")
+        }
+        
+        if leftPointScore > 21, rightPointScore >= 20, distance >= 2 {
+            print("デュース。左側の選手が勝利です。")
+            
+        } else if rightPointScore > 21, leftPointScore >= 20, distance >= 2 {
+            print("デュース。右側の選手が勝利です。")
+            
+        }
+        
+        distance = abs(leftPointScore - rightPointScore)
+        
     }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "左の選手が勝利です。", message: "おめでとうございます！", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "もう一度", style: .default, handler: { (action) in
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func moveToResultScene(player: String) {
+        if player == "left" {
+            performSegue(withIdentifier: "result", sender: [])
+        } else if player == "right" {
+            performSegue(withIdentifier: "result", sender: [])
+        }
+     }
     
 }
 
