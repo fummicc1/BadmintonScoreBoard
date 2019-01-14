949ec3ee4ec723f8e575ba7cfee3fd432cd77bbb
fummicc1-sub <fumiya989@gmail.com>
2019-01-14 16:56:40 +0900

    commit

--- BadmintonScoreBoard_Yudai/ViewController.swift
+++ BadmintonScoreBoard_Yudai/ViewController.swift
@@ -17,17 +17,18 @@ class ViewController: UIViewController {
     var rightGameScore = 0
     var distance: Int = 0
     
+    @IBOutlet var leftScoreButton: UIButton!
+    @IBOutlet var rightScoreButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func tappedLeftButton(sender: UIButton) {
         leftPointScore += 1
         sender.setTitle(String(leftPointScore), for: UIControl.State.normal)
         checkGameset()
     }
-    
     
     @IBAction func tappedRightButton(sender: UIButton) {
         rightPointScore += 1
@@ -39,25 +40,36 @@ class ViewController: UIViewController {
         
         if leftPointScore == 21, rightPointScore <= 19 {
             print("左側の選手が勝利です。")
+            showLeftAlert()
         } else if rightPointScore == 21, leftPointScore <= 19 {
             print("右側の選手が勝利です。")
+            showRightAlert()
         }
         
         if leftPointScore > 21, rightPointScore >= 20, distance >= 2 {
             print("デュース。左側の選手が勝利です。")
-            
+            showLeftAlert()
         } else if rightPointScore > 21, leftPointScore >= 20, distance >= 2 {
             print("デュース。右側の選手が勝利です。")
-            
+            showRightAlert()
         }
         
         distance = abs(leftPointScore - rightPointScore)
         
     }
     
-    func showAlert() {
+    func showRightAlert() {
+        let alert = UIAlertController(title: "右の選手が勝利です。", message: "おめでとうございます！", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "もう一度", style: .default, handler: { (action) in
+            self.resetScore()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showLeftAlert() {
         let alert = UIAlertController(title: "左の選手が勝利です。", message: "おめでとうございます！", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "もう一度", style: .default, handler: { (action) in
+            self.resetScore()
         }))
         present(alert, animated: true, completion: nil)
     }
@@ -69,6 +81,13 @@ class ViewController: UIViewController {
             performSegue(withIdentifier: "result", sender: [])
         }
      }
+    
+    func resetScore() {
+        rightPointScore = 0
+        leftPointScore = 0
+        leftScoreButton.setTitle("\(leftPointScore)", for: UIControl.State.normal)
+        rightScoreButton.setTitle("\(rightPointScore)", for: UIControl.State.normal)
+    }
     
 }
 
