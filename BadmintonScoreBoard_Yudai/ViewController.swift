c5b375f18d54dd53b4ef1fb3e80e5788910ba887
fummicc1-sub <fumiya989@gmail.com>
2019-01-27 20:19:40 +0900

    refactoring

--- BadmintonScoreBoard_Yudai/ViewController.swift
+++ BadmintonScoreBoard_Yudai/ViewController.swift
@@ -15,55 +15,62 @@ class ViewController: UIViewController {
     
     var rightPointScore = 0
     var rightGameScore = 0
-    var distance: Int = 0
     
     @IBOutlet var leftScoreButton: UIButton!
     @IBOutlet var rightScoreButton: UIButton!
+    @IBOutlet var leftGameButton: UIButton!
+    @IBOutlet var rightGameButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func tappedLeftButton(sender: UIButton) {
+    @IBAction func tappedLeftButton() {
         leftPointScore += 1
-        sender.setTitle(String(leftPointScore), for: UIControl.State.normal)
-        checkGameset()
+        leftScoreButton.setTitle(String(leftPointScore), for: UIControl.State.normal)
+        
+        checkGame()
+        
     }
     
-    @IBAction func tappedRightButton(sender: UIButton) {
+    @IBAction func tappedRightButton() {
+        
         rightPointScore += 1
-        sender.setTitle(String(rightPointScore), for: UIControl.State.normal)
-        checkGameset()
+        rightScoreButton.setTitle(String(rightPointScore), for: UIControl.State.normal)
+        
+        checkGame()
+        
     }
-
-    func checkGameset() {
+    
+    func checkGame() {
         
         if leftPointScore == 21, rightPointScore <= 19 {
+            
             print("左側の選手が勝利です。")
+            
             showLeftAlert()
+            
         } else if rightPointScore == 21, leftPointScore <= 19 {
+            
             print("右側の選手が勝利です。")
+            
             showRightAlert()
+            
         }
         
-        if leftPointScore > 21, rightPointScore >= 20, distance >= 2 {
+        if leftPointScore > 21, rightPointScore >= 20, leftPointScore - rightPointScore >= 2 {
+            
             print("デュース。左側の選手が勝利です。")
+            
             showLeftAlert()
-        } else if rightPointScore > 21, leftPointScore >= 20, distance >= 2 {
+            
+        } else if rightPointScore > 21, leftPointScore >= 20, rightPointScore - leftPointScore >= 2 {
+            
             print("デュース。右側の選手が勝利です。")
+            
             showRightAlert()
+            
         }
-        
-        distance = abs(leftPointScore - rightPointScore)
-        
-    }
-    
-    func showRightAlert() {
-        let alert = UIAlertController(title: "右の選手が勝利です。", message: "おめでとうございます！", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "もう一度", style: .default, handler: { (action) in
-            self.resetScore()
-        }))
-        present(alert, animated: true, completion: nil)
     }
     
     func showLeftAlert() {
@@ -73,21 +80,28 @@ class ViewController: UIViewController {
         }))
         present(alert, animated: true, completion: nil)
     }
-    
-    func moveToResultScene(player: String) {
-        if player == "left" {
-            performSegue(withIdentifier: "result", sender: [])
-        } else if player == "right" {
-            performSegue(withIdentifier: "result", sender: [])
-        }
-     }
+
+    func showRightAlert() {
+        let alert = UIAlertController(title: "右の選手が勝利です。", message: "おめでとうございます！", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "もう一度", style: .default, handler: { (action) in
+            self.resetScore()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
     
     func resetScore() {
         rightPointScore = 0
         leftPointScore = 0
-        leftScoreButton.setTitle("\(leftPointScore)", for: UIControl.State.normal)
-        rightScoreButton.setTitle("\(rightPointScore)", for: UIControl.State.normal)
+        leftScoreButton.setTitle(String(self.leftPointScore), for: UIControl.State.normal)
+        rightScoreButton.setTitle(String(self.rightPointScore), for: UIControl.State.normal)
     }
     
 }
 
+//func moveToResultScene(player: String) {
+//    if player == "left" {
+//        performSegue(withIdentifier: "result", sender: [])
+//    } else if player == "right" {
+//        performSegue(withIdentifier: "result", sender: [])
+//    }
+//}
